3c6f22b93a0c45d94c25305d18d664a2be5df169
PathrickIT <dgzmnsa24@gmail.com>
2024-09-14 02:02:13 +0800

    bwahwahawhawh  tanginanioiiyo

---
+++ cam.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace BarosDashboard
+{
+    partial class cam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            camlabel = new Label();
+            comboBox1 = new ComboBox();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // camlabel
+            // 
+            camlabel.AutoSize = true;
+            camlabel.Location = new Point(96, 26);
+            camlabel.Name = "camlabel";
+            camlabel.Size = new Size(48, 15);
+            camlabel.TabIndex = 0;
+            camlabel.Text = "Camera";
+            camlabel.Click += label1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(177, 23);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(240, 23);
+            comboBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(54, 52);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(595, 393);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(707, 52);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(834, 52);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 4;
+            button2.Text = "Read";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(666, 113);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(267, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // cam
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1100, 467);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(camlabel);
+            Name = "cam";
+            Text = "cam";
+            FormClosing += cam_FormClosing;
+            Load += cam_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label camlabel;
+        private ComboBox comboBox1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ cam.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+using ZXing.Aztec;
+using System.Windows.Forms;
+using ZXing.Windows.Compatibility;
+
+
+
+namespace BarosDashboard
+{
+    public partial class cam : Form
+    {
+        private FilterInfoCollection CaptureDevice;
+        private VideoCaptureDevice FinalFrame;
+      
+        public cam()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cam_Load(object sender, EventArgs e)
+        {
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in CaptureDevice)
+                comboBox1.Items.Add(device.Name);
+
+            comboBox1.SelectedIndex = 0;
+            FinalFrame = new VideoCaptureDevice();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[comboBox1.SelectedIndex].MonikerString);
+            FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+            FinalFrame.Start();
+        }
+
+        private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+
+        }
+
+        private void cam_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (FinalFrame.IsRunning == true)
+            {
+                FinalFrame.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                var reader = new ZXing.BarcodeReader<Bitmap>(null, bmp => new BitmapLuminanceSource(bmp), luminance => new ZXing.Common.HybridBinarizer(luminance));
+
+                Result result = reader.Decode((Bitmap)pictureBox1.Image);
+                try
+                {
+                    if (result != null)
+                    {
+                        string decoded = result.Text.Trim();
+                        if (!string.IsNullOrEmpty(decoded))
+                        {
+                            textBox1.Text = decoded;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Handle exception
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start(); 
+        }
+    }
+}
