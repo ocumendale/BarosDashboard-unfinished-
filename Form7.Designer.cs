c0381cb32c8b78ae891ff7e8a9146852aaad2bb2
PathrickIT <dgzmnsa24@gmail.com>
2024-09-14 15:52:43 +0800

    ano to

--- Form7.Designer.cs
+++ Form7.Designer.cs
@@ -44,10 +44,9 @@
             // signUpLogo
             // 
             signUpLogo.Image = (Image)resources.GetObject("signUpLogo.Image");
-            signUpLogo.Location = new Point(438, 128);
-            signUpLogo.Margin = new Padding(3, 4, 3, 4);
+            signUpLogo.Location = new Point(383, 96);
             signUpLogo.Name = "signUpLogo";
-            signUpLogo.Size = new Size(362, 400);
+            signUpLogo.Size = new Size(317, 300);
             signUpLogo.SizeMode = PictureBoxSizeMode.StretchImage;
             signUpLogo.TabIndex = 26;
             signUpLogo.TabStop = false;
@@ -56,9 +55,9 @@
             // 
             exit.AutoSize = true;
             exit.Font = new Font("MS UI Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            exit.Location = new Point(206, 345);
+            exit.Location = new Point(180, 259);
             exit.Name = "exit";
-            exit.Size = new Size(65, 30);
+            exit.Size = new Size(52, 24);
             exit.TabIndex = 25;
             exit.Text = "Exit";
             exit.Click += exit_Click;
@@ -68,10 +67,9 @@
             clear2_btn.BackColor = Color.DarkGoldenrod;
             clear2_btn.FlatStyle = FlatStyle.Flat;
             clear2_btn.ForeColor = Color.FromArgb(24, 25, 56);
-            clear2_btn.Location = new Point(114, 280);
-            clear2_btn.Margin = new Padding(3, 4, 3, 4);
+            clear2_btn.Location = new Point(100, 210);
             clear2_btn.Name = "clear2_btn";
-            clear2_btn.Size = new Size(114, 47);
+            clear2_btn.Size = new Size(100, 35);
             clear2_btn.TabIndex = 24;
             clear2_btn.Text = "Clear";
             clear2_btn.UseVisualStyleBackColor = false;
@@ -83,10 +81,9 @@
             signin_Btn.FlatAppearance.BorderSize = 0;
             signin_Btn.FlatStyle = FlatStyle.Flat;
             signin_Btn.ForeColor = Color.FromArgb(246, 180, 71);
-            signin_Btn.Location = new Point(247, 280);
-            signin_Btn.Margin = new Padding(3, 4, 3, 4);
+            signin_Btn.Location = new Point(216, 210);
             signin_Btn.Name = "signin_Btn";
-            signin_Btn.Size = new Size(114, 47);
+            signin_Btn.Size = new Size(100, 35);
             signin_Btn.TabIndex = 23;
             signin_Btn.Text = "Sign In";
             signin_Btn.UseVisualStyleBackColor = false;
@@ -98,19 +95,18 @@
             cNum_tb.BorderStyle = BorderStyle.FixedSingle;
             cNum_tb.Cursor = Cursors.IBeam;
             cNum_tb.ForeColor = Color.FromArgb(24, 25, 56);
-            cNum_tb.Location = new Point(114, 155);
-            cNum_tb.Margin = new Padding(3, 4, 3, 4);
+            cNum_tb.Location = new Point(100, 116);
             cNum_tb.Multiline = true;
             cNum_tb.Name = "cNum_tb";
-            cNum_tb.Size = new Size(247, 37);
+            cNum_tb.Size = new Size(216, 28);
             cNum_tb.TabIndex = 22;
             // 
             // contactNum
             // 
             contactNum.AutoSize = true;
-            contactNum.Location = new Point(114, 128);
+            contactNum.Location = new Point(100, 96);
             contactNum.Name = "contactNum";
-            contactNum.Size = new Size(118, 20);
+            contactNum.Size = new Size(96, 15);
             contactNum.TabIndex = 21;
             contactNum.Text = "Contact Number";
             // 
@@ -120,19 +116,19 @@
             pass_tb.BorderStyle = BorderStyle.FixedSingle;
             pass_tb.Cursor = Cursors.IBeam;
             pass_tb.ForeColor = Color.FromArgb(24, 25, 56);
-            pass_tb.Location = new Point(114, 223);
-            pass_tb.Margin = new Padding(3, 4, 3, 4);
+            pass_tb.Location = new Point(100, 167);
             pass_tb.Multiline = true;
             pass_tb.Name = "pass_tb";
-            pass_tb.Size = new Size(247, 37);
+            pass_tb.PasswordChar = '*';
+            pass_tb.Size = new Size(216, 28);
             pass_tb.TabIndex = 20;
             // 
             // passWord
             // 
             passWord.AutoSize = true;
-            passWord.Location = new Point(114, 196);
+            passWord.Location = new Point(100, 147);
             passWord.Name = "passWord";
-            passWord.Size = new Size(74, 20);
+            passWord.Size = new Size(60, 15);
             passWord.TabIndex = 19;
             passWord.Text = "Password ";
             // 
@@ -140,18 +136,18 @@
             // 
             loginlbl.AutoSize = true;
             loginlbl.Font = new Font("MS UI Gothic", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            loginlbl.Location = new Point(344, 72);
+            loginlbl.Location = new Point(301, 54);
             loginlbl.Name = "loginlbl";
-            loginlbl.Size = new Size(129, 37);
+            loginlbl.Size = new Size(102, 29);
             loginlbl.TabIndex = 18;
             loginlbl.Text = "Sign In";
             // 
             // login
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(246, 180, 71);
-            ClientSize = new Size(914, 600);
+            ClientSize = new Size(800, 450);
             Controls.Add(signUpLogo);
             Controls.Add(exit);
             Controls.Add(clear2_btn);
@@ -161,7 +157,6 @@
             Controls.Add(pass_tb);
             Controls.Add(passWord);
             Controls.Add(loginlbl);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "login";
             Text = "Login";
             Load += login_Load;
