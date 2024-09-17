b5014e8b45653b5db9f1dab65adf14ca060291cf
Nathaniel Santos <nathanielsantos02039@gmail.com>
2024-09-18 02:48:57 +0800

    lol

--- Form7.Designer.cs
+++ Form7.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace BarosDashboard
+﻿
+namespace BarosDashboard
 {
     partial class login
     {
@@ -33,9 +34,9 @@
             exit = new Label();
             clear2_btn = new Button();
             signin_Btn = new Button();
-            cNum_tb = new TextBox();
+            txt_ContactNum = new TextBox();
             contactNum = new Label();
-            pass_tb = new TextBox();
+            txt_password = new TextBox();
             passWord = new Label();
             loginlbl = new Label();
             ((System.ComponentModel.ISupportInitialize)signUpLogo).BeginInit();
@@ -45,9 +46,10 @@
             // 
             signUpLogo.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             signUpLogo.Image = (Image)resources.GetObject("signUpLogo.Image");
-            signUpLogo.Location = new Point(383, 96);
+            signUpLogo.Location = new Point(438, 128);
+            signUpLogo.Margin = new Padding(3, 4, 3, 4);
             signUpLogo.Name = "signUpLogo";
-            signUpLogo.Size = new Size(317, 300);
+            signUpLogo.Size = new Size(362, 400);
             signUpLogo.SizeMode = PictureBoxSizeMode.StretchImage;
             signUpLogo.TabIndex = 26;
             signUpLogo.TabStop = false;
@@ -56,9 +58,9 @@
             // 
             exit.AutoSize = true;
             exit.Font = new Font("MS UI Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            exit.Location = new Point(180, 259);
+            exit.Location = new Point(206, 345);
             exit.Name = "exit";
-            exit.Size = new Size(52, 24);
+            exit.Size = new Size(65, 30);
             exit.TabIndex = 25;
             exit.Text = "Exit";
             exit.Click += exit_Click;
@@ -68,9 +70,10 @@
             clear2_btn.BackColor = Color.DarkGoldenrod;
             clear2_btn.FlatStyle = FlatStyle.Flat;
             clear2_btn.ForeColor = Color.FromArgb(24, 25, 56);
-            clear2_btn.Location = new Point(100, 210);
+            clear2_btn.Location = new Point(114, 280);
+            clear2_btn.Margin = new Padding(3, 4, 3, 4);
             clear2_btn.Name = "clear2_btn";
-            clear2_btn.Size = new Size(100, 35);
+            clear2_btn.Size = new Size(114, 47);
             clear2_btn.TabIndex = 24;
             clear2_btn.Text = "Clear";
             clear2_btn.UseVisualStyleBackColor = false;
@@ -82,54 +85,58 @@
             signin_Btn.FlatAppearance.BorderSize = 0;
             signin_Btn.FlatStyle = FlatStyle.Flat;
             signin_Btn.ForeColor = Color.FromArgb(246, 180, 71);
-            signin_Btn.Location = new Point(216, 210);
+            signin_Btn.Location = new Point(247, 280);
+            signin_Btn.Margin = new Padding(3, 4, 3, 4);
             signin_Btn.Name = "signin_Btn";
-            signin_Btn.Size = new Size(100, 35);
+            signin_Btn.Size = new Size(114, 47);
             signin_Btn.TabIndex = 23;
             signin_Btn.Text = "Sign In";
             signin_Btn.UseVisualStyleBackColor = false;
             signin_Btn.Click += signin_Btn_Click;
             // 
-            // cNum_tb
+            // txt_ContactNum
             // 
-            cNum_tb.BackColor = Color.DarkGoldenrod;
-            cNum_tb.BorderStyle = BorderStyle.FixedSingle;
-            cNum_tb.Cursor = Cursors.IBeam;
-            cNum_tb.ForeColor = Color.FromArgb(24, 25, 56);
-            cNum_tb.Location = new Point(100, 116);
-            cNum_tb.Multiline = true;
-            cNum_tb.Name = "cNum_tb";
-            cNum_tb.Size = new Size(216, 28);
-            cNum_tb.TabIndex = 22;
+            txt_ContactNum.BackColor = Color.DarkGoldenrod;
+            txt_ContactNum.BorderStyle = BorderStyle.FixedSingle;
+            txt_ContactNum.Cursor = Cursors.IBeam;
+            txt_ContactNum.ForeColor = Color.FromArgb(24, 25, 56);
+            txt_ContactNum.Location = new Point(114, 155);
+            txt_ContactNum.Margin = new Padding(3, 4, 3, 4);
+            txt_ContactNum.Multiline = true;
+            txt_ContactNum.Name = "txt_ContactNum";
+            txt_ContactNum.Size = new Size(247, 37);
+            txt_ContactNum.TabIndex = 22;
+            txt_ContactNum.TextChanged += cNum_tb_TextChanged;
             // 
             // contactNum
             // 
             contactNum.AutoSize = true;
-            contactNum.Location = new Point(100, 96);
+            contactNum.Location = new Point(114, 128);
             contactNum.Name = "contactNum";
-            contactNum.Size = new Size(96, 15);
+            contactNum.Size = new Size(118, 20);
             contactNum.TabIndex = 21;
             contactNum.Text = "Contact Number";
             // 
-            // pass_tb
+            // txt_password
             // 
-            pass_tb.BackColor = Color.DarkGoldenrod;
-            pass_tb.BorderStyle = BorderStyle.FixedSingle;
-            pass_tb.Cursor = Cursors.IBeam;
-            pass_tb.ForeColor = Color.FromArgb(24, 25, 56);
-            pass_tb.Location = new Point(100, 167);
-            pass_tb.Multiline = true;
-            pass_tb.Name = "pass_tb";
-            pass_tb.PasswordChar = '*';
-            pass_tb.Size = new Size(216, 28);
-            pass_tb.TabIndex = 20;
+            txt_password.BackColor = Color.DarkGoldenrod;
+            txt_password.BorderStyle = BorderStyle.FixedSingle;
+            txt_password.Cursor = Cursors.IBeam;
+            txt_password.ForeColor = Color.FromArgb(24, 25, 56);
+            txt_password.Location = new Point(114, 223);
+            txt_password.Margin = new Padding(3, 4, 3, 4);
+            txt_password.Multiline = true;
+            txt_password.Name = "txt_password";
+            txt_password.PasswordChar = '*';
+            txt_password.Size = new Size(247, 37);
+            txt_password.TabIndex = 20;
             // 
             // passWord
             // 
             passWord.AutoSize = true;
-            passWord.Location = new Point(100, 147);
+            passWord.Location = new Point(114, 196);
             passWord.Name = "passWord";
-            passWord.Size = new Size(60, 15);
+            passWord.Size = new Size(74, 20);
             passWord.TabIndex = 19;
             passWord.Text = "Password ";
             // 
@@ -138,27 +145,28 @@
             loginlbl.Anchor = AnchorStyles.Top;
             loginlbl.AutoSize = true;
             loginlbl.Font = new Font("MS UI Gothic", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            loginlbl.Location = new Point(301, 54);
+            loginlbl.Location = new Point(344, 72);
             loginlbl.Name = "loginlbl";
-            loginlbl.Size = new Size(102, 29);
+            loginlbl.Size = new Size(129, 37);
             loginlbl.TabIndex = 18;
             loginlbl.Text = "Sign In";
             // 
             // login
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(246, 180, 71);
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(signUpLogo);
             Controls.Add(exit);
             Controls.Add(clear2_btn);
             Controls.Add(signin_Btn);
-            Controls.Add(cNum_tb);
+            Controls.Add(txt_ContactNum);
             Controls.Add(contactNum);
-            Controls.Add(pass_tb);
+            Controls.Add(txt_password);
             Controls.Add(passWord);
             Controls.Add(loginlbl);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "login";
             Text = "Login";
             Load += login_Load;
@@ -167,15 +175,25 @@
             PerformLayout();
         }
 
+        private void exit_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void cNum_tb_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
         #endregion
 
         private PictureBox signUpLogo;
         private Label exit;
         private Button clear2_btn;
         private Button signin_Btn;
-        private TextBox cNum_tb;
+        private TextBox txt_ContactNum;
         private Label contactNum;
-        private TextBox pass_tb;
+        private TextBox txt_password;
         private Label passWord;
         private Label loginlbl;
     }
--- Form7.cs
+++ Form7.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace BarosDashboard
 {
@@ -18,6 +19,8 @@ namespace BarosDashboard
             InitializeComponent();
         }
 
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-0VUJ82CU;Initial Catalog=userregcs;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+
         private void login_Load(object sender, EventArgs e)
         {
 
@@ -25,30 +28,55 @@ namespace BarosDashboard
 
         private void clear2_btn_Click(object sender, EventArgs e)
         {
-            cNum_tb.Clear();
-            pass_tb.Clear();
+            txt_ContactNum.Clear();
+            txt_password.Clear();
+
+            txt_ContactNum.Focus();
         }
 
         private void signin_Btn_Click(object sender, EventArgs e)
         {
-            Form1 form1 = new Form1();
-            form1.Show();
-            Visible = false;
+            String username, user_password;
+            username = txt_ContactNum.Text;
+            user_password = txt_password.Text;
 
+            try
+            {
+                con.Open();
+                String query = "SELECT * FROM register WHERE contactNumber = @contactNumber AND password = @password";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+                adapter.SelectCommand.Parameters.AddWithValue("@contactNumber", txt_ContactNum.Text);
+                adapter.SelectCommand.Parameters.AddWithValue("@password", txt_password.Text);
+
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    username = txt_ContactNum.Text;
+                    user_password = txt_password.Text;
+
+                    Form1 form1 = new Form1();
+                    form1.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Login Details", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt_ContactNum.Clear();
+                    txt_password.Clear();
+
+                    txt_ContactNum.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
         }
-
-        private void exit_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-        private bool IsValidUsername(string username)
-        {
-
-            return !string.IsNullOrEmpty(username);
-        }
-
-        
-
-
     }
 }
